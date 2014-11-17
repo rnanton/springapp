84dcbf16e26d4ae5c61ff36e07f2b36413b45c5a
Ross Nanton <rnanton@zappos.com>
2014-11-16 16:06:22 -0800

    And more changes...

--- RossSpringApp/src/springapp/web/InventoryController.java
+++ RossSpringApp/src/springapp/web/InventoryController.java
@@ -10,20 +10,36 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import springapp.service.ProductManager;
+
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
 import java.util.Date;
 
-public class HelloController implements Controller {
+public class InventoryController implements Controller {
 
     protected final Log logger = LogFactory.getLog(getClass());
+    
+    private ProductManager productManager;
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String now = (new Date()).toString();
         logger.info("Returning hello view with " + now);
+        
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
 
-        return new ModelAndView("hello", "now", now);
+        return new ModelAndView("hello", "model", myModel);        
+
     }
+    
+    public void setProductManager(ProductManager productManager) {
+        this.productManager = productManager;
+    }    
 
 }
